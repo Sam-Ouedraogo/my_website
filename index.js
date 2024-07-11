b3d749bc3353291873e42334b267fc2775de3c40
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2024-07-11 07:24:55 -0400

    script.js edited

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
-function toggleMenu () {
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
-}
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files (like CSS, JavaScript)
+app.use(express.static(path.join(__dirname, 'my_website')));
+
+// Route to serve the HTML file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'my_website', 'index.html'));
+});
+
+app.listen(PORT, () => {
+    //console.log(`Server is running on port ${PORT}`);
+});
