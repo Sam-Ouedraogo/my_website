11ca872c4d90bda96b2e60a5d457dc4e17d5fe3d
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2024-09-08 19:51:51 -0400

    paths updated

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8001;
 
 // Serve static files (like CSS, JavaScript)
-app.use(express.static(path.join(__dirname, 'my_website')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Route to serve the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'my_website', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
-    //console.log(`Server is running on port ${PORT}`);
+  //console.log(`Server is running on port ${PORT}`);
 });
