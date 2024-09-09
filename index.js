2187f4e8a28eee2f01c895b8f0522e6017923a2a
Yasuke Ouedraogo <yasukeouedraogo@Yasukes-iMac.local>
2024-09-08 20:39:07 -0400

    root path corrected

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8001;
 app.use(express.static(path.join(__dirname, "public")));
 
 // Route to serve the HTML file
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
